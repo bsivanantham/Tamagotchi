48278f900442739bb6d0921433d394a55277cb37
Balavivek Sivanantham <bsivanantham@techfak.uni-bielefeld.de>
2018-11-07 12:50:23 +0100

    feedback page

--- Tamgotchi/app/src/main/java/com/socialrobotics/tamgotchi/MainActivity.java
+++ Tamgotchi/app/src/main/java/com/socialrobotics/tamgotchi/MainActivity.java
@@ -1,8 +1,11 @@
 package com.socialrobotics.tamgotchi;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,6 +24,17 @@ public class MainActivity extends NAO {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Feed back", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
         mFirebaseInstance = FirebaseDatabase.getInstance();
 
         // get reference to 'users' node
